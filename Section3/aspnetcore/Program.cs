b87167abb7f8dbf9aab8ac49aac6eb8d7ff354ad
neighborpil <neighborpil@naver.com>
2019-08-12 00:35:38 +0900

    secton3-lec16 finished

---
+++ Section3/aspnetcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".net core!!");
+        }
+    }
+}
